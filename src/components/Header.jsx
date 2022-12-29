6064f68eae3fb0d76ebbdcaf5849314dacdfb0be
Remmy Abubakar <remmybello123@gmail.com>
2022-12-29 17:32:04 +0000

    darkmode icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,32 @@
 import React from "react";
+import { CgDarkMode } from "react-icons/cg";
+import { CiLight } from "react-icons/ci";
 
 function Header({ handleToggleDarkMode, darkMode }) {
   return (
     <div className="header flex items-center justify-between mb-10">
-      <h1 className={`font-bold text-4xl mt-10 ${darkMode ? "dark:text-white" : ""}`}>Notes</h1>
+      <h1
+        className={`font-bold text-4xl mt-10 ${
+          darkMode ? "dark:text-white" : ""
+        }`}
+      >
+        Notes
+      </h1>
       <button
         onClick={() =>
           handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
         }
-        className="save bg-[#e1e1e1] border-none rounded-[15px] p-[5px_10px_5px_10px] hover:bg-[#ededed] hover:cursor-pointer mt-10"
+        className={
+          darkMode
+            ? "save bg-[#e1e1e1] border-none rounded-[15px] p-[5px_10px_5px_10px] hover:bg-[#ededed] hover:cursor-pointer mt-10"
+            : ""
+        }
       >
-        Toggle Mode
+        {darkMode ? (
+          <CiLight size={35} />
+        ) : (
+          <CgDarkMode size={35} className="hover:cursor-pointer mt-10" />
+        )}
       </button>
     </div>
   );
